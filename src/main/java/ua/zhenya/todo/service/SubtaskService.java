2d6661a50c0022f4dbda2bc7e1fc55c8cf55d8ed
Zheka Slepko <zhenya.slepko@gmail.com>
2024-07-03 20:14:24 +0300

    feat: реализовать 2 метода в SubtaskService

---
+++ src/main/java/ua/zhenya/todo/service/SubtaskService.java
@@ -0,0 +1,57 @@
+package ua.zhenya.todo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.zhenya.todo.dto.task.TaskCreateDTO;
+import ua.zhenya.todo.dto.task.TaskReadDTO;
+import ua.zhenya.todo.mappers.task.TaskCreateMapper;
+import ua.zhenya.todo.mappers.task.TaskReadMapper;
+import ua.zhenya.todo.model.Task;
+import ua.zhenya.todo.model.User;
+import ua.zhenya.todo.repository.TaskRepository;
+import ua.zhenya.todo.repository.UserRepository;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SubtaskService {
+    private final TaskService taskService;
+    private final TaskRepository taskRepository;
+    private final UserService userService;
+    private final TaskCreateMapper taskCreateMapper;
+
+    @Transactional
+    public Task create(Principal principal, Integer parentTaskId, TaskCreateDTO createDTO) {
+        User user = userService.findByUsername(principal.getName());
+        Task parentTask = taskService.findById(principal, parentTaskId);
+
+        return Optional.of(createDTO)
+                .map(taskCreateMapper::map)
+                .map(subtask -> {
+                    subtask.setParentTask(parentTask);
+
+                    subtask.setUser(user);
+                    return subtask;
+                })
+                .map(taskRepository::save)
+                .orElseThrow();
+    }
+
+    public Page<Task> findAll(Principal principal, Integer parentTaskId, Pageable pageable) {
+        User user = userService.findByUsername(principal.getName());
+        Task parentTask = taskService.findById(parentTaskId);
+        taskService.verifyTaskOwner(parentTask, user);
+
+        return taskRepository.findAllByParentTaskId(parentTaskId, pageable);
+
+    }
+
+
+}
