db0ffb8c49f1dd8a76eb203ea71ca72809402a77
Zheka Slepko <zhenya.slepko@gmail.com>
2024-07-03 20:11:52 +0300

    refactor: разделить метод findById на 3 вспомогательных

--- src/main/java/ua/zhenya/todo/service/TaskService.java
+++ src/main/java/ua/zhenya/todo/service/TaskService.java
@@ -3,21 +3,15 @@ package ua.zhenya.todo.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.zhenya.todo.dto.task.TaskCreateDTO;
-import ua.zhenya.todo.dto.task.TaskReadDTO;
-import ua.zhenya.todo.dto.task.TaskUpdateDTO;
 import ua.zhenya.todo.mappers.task.TaskCreateMapper;
-import ua.zhenya.todo.mappers.task.TaskReadMapper;
-import ua.zhenya.todo.mappers.user.UserCreateMapper;
 import ua.zhenya.todo.model.Task;
 import ua.zhenya.todo.model.User;
 import ua.zhenya.todo.repository.TaskRepository;
 
 import java.security.Principal;
-import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,17 +21,36 @@ public class TaskService {
     private final UserService userService;
     private final TaskCreateMapper taskCreateMapper;
 
-    public Task findById(Principal principal, Integer id) {
+/*    public Task findById(Principal principal, Integer id) {
         User user = userService.findByUsername(principal.getName());
 
         return taskRepository.findById(id)
                 .map(task -> {
-                    if (!task.getUser().getId().equals(user.getId())) {
-                        throw new IllegalArgumentException("Задача с id:" + id + " не принадлежит текущему пользователю!");
-                    }
+                    verifyTaskOwner(task, user);
                     return task;
                 })
                 .orElseThrow(() -> new IllegalArgumentException("Задача с id:" + id + " не найдена!"));
+    }*/
+
+    public Task findById(Integer id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Задача с id:" + id + " не найдена!"));
+    }
+
+    public Task findById(Principal principal, Integer id) {
+        User user = userService.findByUsername(principal.getName());
+
+        Task task = findById(id);
+        verifyTaskOwner(task, user);
+
+        return task;
+    }
+
+
+    public void verifyTaskOwner(Task task, User user) {
+        if (!task.getUser().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("Вы не можете этого сделать!");
+        }
     }
 
 
@@ -55,9 +68,9 @@ public class TaskService {
         return taskRepository.save(task);
     }
 
-    public Task update(Principal principal, Integer id, TaskUpdateDTO taskUpdateDTO) {
+/*    public Task update(Principal principal, Integer id, TaskUpdateDTO taskUpdateDTO) {
 
-    }
+    }*/
 
     @Transactional
     public boolean delete(Principal principal, Integer id) {
@@ -65,9 +78,7 @@ public class TaskService {
 
         return taskRepository.findById(id)
                 .map(task -> {
-                    if (!task.getUser().getId().equals(user.getId())) {
-                        throw new UnsupportedOperationException("Вы не можете удалить эту задачу!");
-                    }
+                    verifyTaskOwner(task, user);
                     taskRepository.delete(task);
                     taskRepository.flush();
                     return true;
