fb20538e6d686bc4fdd29df9935c96c42ca9920e
Zheka Slepko <zhenya.slepko@gmail.com>
2024-07-09 13:14:19 +0300

    feat: создать сущность ChecklistItem

---
+++ src/main/java/ua/zhenya/todo/model/ChecklistItem.java
@@ -0,0 +1,29 @@
+package ua.zhenya.todo.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChecklistItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private boolean completed;
+    private LocalDate targetDate;
+    private LocalTime targetTime;
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+}
